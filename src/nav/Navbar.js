1e3c802566f700df6bff48d7e49afbe3beb862da
mlndz-la <erickmlndz05@gmail.com>
2020-10-10 14:48:24 -0700

    Navbar.js has access to its css file

--- src/nav/Navbar.js
+++ src/nav/Navbar.js
@@ -1,9 +1,13 @@
 import React, {  } from 'react';
 
+import './css/navbar.css';
+
 const Navbar = () => {
   return (
       <div>
-        
+        <div className='navbar-container'>
+          
+        </div>
       </div>
   );
 }
