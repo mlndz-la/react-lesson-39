60f8c87c76989b209ae65b6310c08a28ce43dd72
mlndz-la <erickmlndz05@gmail.com>
2020-10-10 14:58:27 -0700

    created structure and connected css to Navbar.js

--- src/nav/Navbar.js
+++ src/nav/Navbar.js
@@ -1,12 +1,16 @@
 import React, {  } from 'react';
 
+import TopNavbar from './top-navbar/TopNavbar';
+import BodyNavbar from './body-navbar/BodyNavbar';
+
 import './css/navbar.css';
 
 const Navbar = () => {
   return (
       <div>
         <div className='navbar-container'>
-          
+          <TopNavbar />
+          <BodyNavbar />
         </div>
       </div>
   );
---
+++ src/nav/top-navbar/TopNavbar.js
@@ -0,0 +1,13 @@
+import React, {  } from 'react';
+
+import '../css/top-navbar.css';
+
+const TopNavbar = () => {
+  return (
+      <div>
+        
+      </div>
+  );
+}
+
+export default TopNavbar;
